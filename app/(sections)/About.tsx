34a186206e4af66317086102854d4c87db58cf0f
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-06-11 13:00:58 +0800

    done: conflict

--- app/(sections)/About.tsx
+++ app/(sections)/About.tsx
@@ -12,16 +12,16 @@ const Home = () => {
             About Me
           </h2>
           <p className="text-gray-800 dark:text-gray-300 text-xl max-w-[700px]">
-            I'm a frontend developer with a passion for creating beautiful and
+            Im a frontend developer with a passion for creating beautiful and
             functional web applications. I have bit experience with flutter
-            native, react js, and next js but I'm willing to learn more to
+            native, react js, and next js but Im willing to learn more to
             enhance my skill and techstacks.
           </p>
           <p className="text-gray-800 dark:text-gray-300 text-xl max-w-[700px]">
             In my free time, I enjoy exploring new design trends, learning about
             the latest advancements in web development, and contributing to
-            open-source projects. I'm always eager to collaborate with others
-            and learn from their experiences.
+            open-source projects. Im always eager to collaborate with others and
+            learn from their experiences.
           </p>
         </div>
       </div>
